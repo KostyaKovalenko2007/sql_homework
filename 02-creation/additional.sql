75e60b46569812e0d510f767baa1d2c25c663917
KostyaKovalenko2007 <117159834+KostyaKovalenko2007@users.noreply.github.com>
2022-12-19 23:27:59 +0300

    Update additional.sql

--- 02-creation/additional.sql
+++ 02-creation/additional.sql
@@ -1,8 +1,19 @@
-CREATE TABLE org_structure (
+
+-- реализация через check
+CREATE TABLE IF NOT EXISTS org_structure (
 	id uuid NOT NULL DEFAULT uuid_generate_v1(),
 	emp_name varchar NOT NULL,
 	dep_name varchar NOT NULL,
 	reports_to uuid NOT NULL,
 	CONSTRAINT org_structure_check CHECK (reports_to = id),
+	CONSTRAINT org_structure_pk PRIMARY KEY (id)
+);
+
+--Реализация через внешний ключ :)
+CREATE TABLE IF NOT exists org_structure (
+	id uuid NOT NULL DEFAULT uuid_generate_v1(),
+	emp_name varchar NOT NULL,
+	dep_name varchar NOT NULL,
+	reports_to uuid NOT null references org_structure(id),
 	CONSTRAINT org_structure_pk PRIMARY KEY (id)
 );
